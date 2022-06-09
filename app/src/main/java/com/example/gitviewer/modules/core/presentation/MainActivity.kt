f2511dd695ff41ae260e4437f2f70f8e509ff039
Obokradut <Visond@rambler.ru>
2022-06-09 10:52:27 +0700

    Added libraries to Gradle. Basic project setup.

---
+++ app/src/main/java/com/example/gitviewer/modules/core/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gitviewer.modules.core.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gitviewer.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
