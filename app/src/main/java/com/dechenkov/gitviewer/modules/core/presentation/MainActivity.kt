73b8c7610893bee7487b5317e7d1023e2590a0c4
Obokradut <Visond@rambler.ru>
2022-06-23 11:31:02 +0700

    Navigation by fragments has been developed

--- app/src/main/java/com/dechenkov/gitviewer/modules/core/presentation/MainActivity.kt
+++ app/src/main/java/com/dechenkov/gitviewer/modules/core/presentation/MainActivity.kt
@@ -1,12 +1,83 @@
 package com.dechenkov.gitviewer.modules.core.presentation
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import com.dechenkov.gitviewer.R
+import com.dechenkov.gitviewer.databinding.ActivityMainBinding
+import com.dechenkov.gitviewer.modules.core.domain.CoreViewModel
+import com.dechenkov.gitviewer.modules.navigation.launchNavHost
+import com.dechenkov.gitviewer.modules.navigation.setupActionBarWithDestinations
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val coreViewModel: CoreViewModel by viewModels()
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_authorization)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                coreViewModel.coreAction.collect {
+                    when (it) {
+                        is CoreViewModel.Action.ShowDialog -> {
+                            AlertDialog.Builder(this@MainActivity)
+                                .setTitle("Logout")
+                                .setMessage("Are you sure")
+                                .setPositiveButton("Yes") { _, _ ->
+                                    it.onSuccess()
+                                }
+                                .setNegativeButton("No") { _, _ ->
+
+                                }
+                                .create()
+                                .show()
+                        }
+                    }
+                }
+            }
+        }
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.coreNavContainer) as NavHostFragment
+        navController = navHostFragment.navController
+
+        launchNavHost(coreViewModel.mainNavProvider, navController)
+
+
+        setupActionBarWithNavController(
+            this as AppCompatActivity,
+            navController
+        )
+        setupActionBarWithDestinations(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.app_bar, menu)
+        menu?.findItem(R.id.logout_button)?.setOnMenuItemClickListener {
+            coreViewModel.onLogoutClick()
+            true
+        }
+        return super.onCreateOptionsMenu(menu)
     }
 }
