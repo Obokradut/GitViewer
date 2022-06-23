94169dc4700f899cd42189810c8bdd3917aae5ec
Obokradut <Visond@rambler.ru>
2022-06-23 11:24:16 +0700

    Created interface for GitApi

---
+++ app/src/main/java/com/dechenkov/gitviewer/shared/gitApi/IGitApi.kt
@@ -0,0 +1,50 @@
+package com.dechenkov.gitviewer.shared.gitApi
+
+import com.dechenkov.gitviewer.shared.models.GitRepositoryFullInfo
+import com.dechenkov.gitviewer.shared.models.GitRepositoryShortInfo
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface IGitApi {
+    @GET("/user")
+    suspend fun getUserInfo(
+        @Header("Authorization") token: String
+    ) : String
+
+    @GET("/user/repos?per_page=10&page=1&type=all&sort=created&direction=desc")
+    suspend fun getRepositories(
+        @Header("Authorization") token: String
+    ) : List<GitRepositoryShortInfo>
+
+    @GET("repos/{owner}/{repo}")
+    suspend fun getRepoDetails(
+        @Header("Authorization") token: String,
+        @Path("owner") owner: String,
+        @Path("repo") repo: String
+    ) : GitRepositoryFullInfo
+
+    @GET("/repos/{owner}/{repo}/contents/{path}")
+    suspend fun getContent(
+        @Header("Authorization") token: String,
+        @Path("owner") owner: String,
+        @Path("repo") repo: String,
+        @Path("path") path: String
+    ) : String
+
+    @GET("repos/{owner}/{repo}/readme")
+    suspend fun getReadme(
+        @Header("Authorization") token: String,
+        @Path("owner") owner: String,
+        @Path("repo") repo: String,
+    ) : String
+
+    @GET("repos/{owner}/{repo}/readme")
+    suspend fun getReadme(
+        @Header("Authorization") token: String,
+        @Path("owner") owner: String,
+        @Path("repo") repo: String,
+        @Query("ref") branch: String
+    ) : String
+}
