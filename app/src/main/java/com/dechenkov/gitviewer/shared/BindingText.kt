62ac80fe85dd28b75704b4f7518f83435dc95286
Obokradut <Visond@rambler.ru>
2022-06-23 10:39:48 +0700

    Updated EditText

---
+++ app/src/main/java/com/dechenkov/gitviewer/shared/BindingText.kt
@@ -0,0 +1,25 @@
+package com.dechenkov.gitviewer.shared
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+
+fun EditText.bindTextTwoWay(liveData: MutableLiveData<String>, lifecycleOwner: LifecycleOwner){
+    val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            liveData.value = s.toString()
+        }
+        override fun afterTextChanged(s: Editable?) = Unit
+    }
+
+    this.addTextChangedListener(textWatcher)
+
+    liveData.observe(lifecycleOwner) { text ->
+        if (this.text.toString() == text) return@observe
+
+        this.setText(text)
+    }
+}
