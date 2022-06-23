f7dd30aeb507e7d3e7978d40939801495fad2766
Obokradut <Visond@rambler.ru>
2022-06-23 11:54:28 +0700

    Deleted

--- app/src/main/java/com/dechenkov/gitviewer/modules/authorization/domain/AuthorizationViewState.kt
+++
@@ -1,7 +0,0 @@
-package com.dechenkov.gitviewer.modules.authorization.domain
-
-sealed interface AuthorizationViewState {
-    object Loading: AuthorizationViewState
-    object PresentInfo: AuthorizationViewState
-    data class Error(val reason: String): AuthorizationViewState
-}
