725fed73a94b4d55e94937c9bfc996e617175c5e
Obokradut <Visond@rambler.ru>
2022-06-23 10:32:41 +0700

    The AppNavProvider has been developed

---
+++ app/src/main/java/com/dechenkov/gitviewer/modules/navigation/AppNavProvider.kt
@@ -0,0 +1,21 @@
+package com.dechenkov.gitviewer.modules.navigation
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+open class AppNavProvider<D> {
+    private val navigationFlow: MutableStateFlow<D?> = MutableStateFlow(null)
+
+    fun requestNavigationFlow(destinations: D?) {
+        navigationFlow.tryEmit(destinations)
+    }
+
+    fun observeNavigationFlow(scope: CoroutineScope, handler: (D?) -> Unit) {
+        navigationFlow.onEach {
+            handler(it)
+            if(it != null) navigationFlow.tryEmit(null)
+        }.launchIn(scope)
+    }
+}
