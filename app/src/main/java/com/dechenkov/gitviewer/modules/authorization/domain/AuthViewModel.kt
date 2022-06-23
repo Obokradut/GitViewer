1250e61d65849ceac4f5d85e68fc65b95492b066
Obokradut <Visond@rambler.ru>
2022-06-23 10:36:01 +0700

    Developed a viewmodel for the authorization fragment

---
+++ app/src/main/java/com/dechenkov/gitviewer/modules/authorization/domain/AuthViewModel.kt
@@ -0,0 +1,85 @@
+package com.dechenkov.gitviewer.modules.authorization.domain
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dechenkov.gitviewer.modules.authorization.domain.usecases.GetEnterGitTokenUseCase
+import com.dechenkov.gitviewer.modules.authorization.domain.usecases.GetGitTokenUseCase
+import com.dechenkov.gitviewer.modules.authorization.domain.usecases.SignInUseCase
+import com.dechenkov.gitviewer.navigation.domain.usecases.NavigateToListRepositories
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AuthViewModel
+@Inject
+constructor(
+    private val getGitToken: GetGitTokenUseCase,
+    private val getEnterGitToken: GetEnterGitTokenUseCase,
+    private val signIn: SignInUseCase,
+    private val navigateToListRepositories: NavigateToListRepositories
+) : ViewModel() {
+    private val _state: MutableLiveData<State> = MutableLiveData(State.Loading)
+    val state: LiveData<State>
+        get() = _state
+
+    private val _actions: Channel<Action> = Channel(Channel.BUFFERED)
+    val actions: Flow<Action>
+        get() = _actions.receiveAsFlow()
+
+    val token: MutableLiveData<String> = MutableLiveData("")
+
+    init {
+        viewModelScope.launch {
+            try {
+                val gitToken = getGitToken()
+                when (gitToken) {
+                    null -> {
+                        _state.postValue(State.Idle)
+                        return@launch
+                    }
+                }
+                signIn(gitToken!!)
+                navigateToListRepositories()
+            }
+            catch (ex: Exception) {
+                _state.postValue(State.InvalidInput(requireNotNull(ex.message)))
+                _actions.send(Action.ShowError(ex.message.toString()))
+            }
+        }
+    }
+
+    fun onSingButtonPressed() {
+        viewModelScope.launch {
+            _state.postValue(State.Loading)
+            try {
+                if(token.value.isNullOrEmpty()) {
+                    _state.postValue(State.InvalidInput(getEnterGitToken()))
+                    return@launch
+                }
+                signIn(token.value!!)
+                navigateToListRepositories()
+            }
+            catch (ex: Exception) {
+                _state.postValue(State.InvalidInput(requireNotNull(ex.message)))
+                _actions.send(Action.ShowError(ex.message.toString()))
+            }
+        }
+    }
+
+    sealed interface State {
+        object Idle : State
+        object Loading : State
+        data class InvalidInput(val reason: String) : State
+    }
+
+    sealed interface Action {
+        data class ShowError(val message: String) : Action
+        object RouteToMain : Action
+    }
+}
