d09da953ff9023980a1769453108691fc907f254
Obokradut <Visond@rambler.ru>
2022-06-26 18:08:13 +0700

    Name update

--- app/src/main/java/com/dechenkov/gitviewer/shared/usecases/BindingText.kt
+++ app/src/main/java/com/dechenkov/gitviewer/shared/usecases/BindingText.kt
@@ -6,7 +6,7 @@ import android.widget.EditText
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 
-fun EditText.bindTextTwoWay(liveData: MutableLiveData<String>, lifecycleOwner: LifecycleOwner){
+fun EditText.bindText(liveData: MutableLiveData<String>, lifecycleOwner: LifecycleOwner){
     val textWatcher = object : TextWatcher {
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
