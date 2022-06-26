0cb68c9c4e69c92d143cfe8e552cb68a3ba60945
Obokradut <Visond@rambler.ru>
2022-06-26 18:16:14 +0700

    Added logout and navigation

--- app/src/main/java/com/dechenkov/gitviewer/modules/core/domain/CoreViewModel.kt
+++ app/src/main/java/com/dechenkov/gitviewer/modules/core/domain/CoreViewModel.kt
@@ -2,20 +2,22 @@ package com.dechenkov.gitviewer.modules.core.domain
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.dechenkov.gitviewer.modules.authorization.domain.AuthViewModel
-import com.dechenkov.gitviewer.modules.navigation.MainNavProvider
+import com.dechenkov.gitviewer.navigation.domain.IMainDestinations
+import com.dechenkov.gitviewer.navigation.domain.MainNavProvider
+import com.dechenkov.gitviewer.shared.app.IAppRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
 class CoreViewModel
 @Inject
 constructor(
-    val mainNavProvider: MainNavProvider
+    val mainNavProvider: MainNavProvider,
+    private val appRepository: IAppRepository
 ): ViewModel() {
     private val _coreActions: Channel<Action> = Channel()
     val coreAction: Flow<Action>
@@ -25,7 +27,8 @@ constructor(
         viewModelScope.launch {
             _coreActions.send(
                 Action.ShowDialog {
-                    TODO("logout")
+                    appRepository.logout()
+                    mainNavProvider.requestNavigationFlow(IMainDestinations.Authorization)
                 }
             )
         }
