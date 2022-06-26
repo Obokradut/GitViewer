4f638ff87816f3bc4278697c44642ec2e106fd02
Obokradut <Visond@rambler.ru>
2022-06-26 18:07:30 +0700

    Update

--- app/src/main/java/com/dechenkov/gitviewer/modules/authorization/presentation/AuthFragment.kt
+++ app/src/main/java/com/dechenkov/gitviewer/modules/authorization/presentation/AuthFragment.kt
@@ -1,26 +1,20 @@
 package com.dechenkov.gitviewer.modules.authorization.presentation
 
-import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.dechenkov.gitviewer.R
 import com.dechenkov.gitviewer.databinding.FragmentAuthorizationBinding
 import com.dechenkov.gitviewer.modules.authorization.domain.AuthViewModel
-import com.dechenkov.gitviewer.shared.bindTextTwoWay
+import com.dechenkov.gitviewer.shared.bindText
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class AuthFragment : Fragment() {
-    private lateinit var binding: FragmentAuthorizationBinding
+    private lateinit var authorizationBinding: FragmentAuthorizationBinding
     private val authViewModel: AuthViewModel by viewModels()
 
     override fun onCreateView(
@@ -28,40 +22,28 @@ class AuthFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentAuthorizationBinding.inflate(inflater, container, false)
+        authorizationBinding = FragmentAuthorizationBinding.inflate(inflater, container, false)
 
-        with(binding){
+        with(authorizationBinding) {
             authViewModel.state.observe(viewLifecycleOwner) {
-                authButton.isEnabled = it !is AuthViewModel.State.Loading
-                authButton.text = if (it is AuthViewModel.State.Loading) "" else getText(R.string.sign_in)
-                authButtonProgressbar.visibility =
-                    if(it is AuthViewModel.State.Loading) View.VISIBLE else View.GONE
-                tokenField.error = if (it is AuthViewModel.State.InvalidInput) it.reason else ""
+                if (it is AuthViewModel.State.Loading) {
+                    authButton.isEnabled = false
+                    authButton.text = ""
+                    authButtonProgressbar.visibility = View.VISIBLE
+                } else {
+                    authButton.isEnabled = true
+                    authButton.text = getText(R.string.sign_in)
+                    authButtonProgressbar.visibility = View.GONE
+                }
+                tokenField.error =
+                    if (it is AuthViewModel.State.InvalidInput) it.reason
+                    else ""
             }
             authButton.setOnClickListener {
                 authViewModel.onSingButtonPressed()
             }
-
-            tokenField.editText?.bindTextTwoWay(authViewModel.token, this@AuthFragment)
+            tokenField.editText?.bindText(authViewModel.token, this@AuthFragment)
         }
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        lifecycleScope.launch {
-            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                authViewModel.actions.collect {
-                    if (it is AuthViewModel.Action.ShowError) {
-                        AlertDialog.Builder(requireContext())
-                            .setPositiveButton("Ok") { _,_ -> }
-                            .setMessage(it.message)
-                            .setTitle("Error")
-                            .create()
-                            .show()
-                    }
-                }
-            }
-        }
+        return authorizationBinding.root
     }
 }
