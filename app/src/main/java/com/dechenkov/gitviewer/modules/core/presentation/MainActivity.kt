056ab317f5ae54122ebae03d113f035686c698f5
Obokradut <Visond@rambler.ru>
2022-06-26 18:20:06 +0700

    Texts update

--- app/src/main/java/com/dechenkov/gitviewer/modules/core/presentation/MainActivity.kt
+++ app/src/main/java/com/dechenkov/gitviewer/modules/core/presentation/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.dechenkov.gitviewer.modules.core.presentation
 
 import android.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.Menu
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -15,22 +17,24 @@ import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import com.dechenkov.gitviewer.R
 import com.dechenkov.gitviewer.databinding.ActivityMainBinding
 import com.dechenkov.gitviewer.modules.core.domain.CoreViewModel
-import com.dechenkov.gitviewer.modules.navigation.launchNavHost
-import com.dechenkov.gitviewer.modules.navigation.setupActionBarWithDestinations
+import com.dechenkov.gitviewer.navigation.domain.launchNavHost
+import com.dechenkov.gitviewer.navigation.domain.setupActionBarWithDestinations
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var activityMainBinding: ActivityMainBinding
     private val coreViewModel: CoreViewModel by viewModels()
+
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
 
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
@@ -38,12 +42,12 @@ class MainActivity : AppCompatActivity() {
                     when (it) {
                         is CoreViewModel.Action.ShowDialog -> {
                             AlertDialog.Builder(this@MainActivity)
-                                .setTitle("Logout")
-                                .setMessage("Are you sure")
-                                .setPositiveButton("Yes") { _, _ ->
+                                .setTitle(getString(R.string.logout))
+                                .setMessage(getString(R.string.are_you_sure))
+                                .setPositiveButton(getString(R.string.yes)) { _, _ ->
                                     it.onSuccess()
                                 }
-                                .setNegativeButton("No") { _, _ ->
+                                .setNegativeButton(getString(R.string.no)) { _, _ ->
 
                                 }
                                 .create()
@@ -56,8 +60,9 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.coreNavContainer) as NavHostFragment
-        navController = navHostFragment.navController
 
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration.Builder(navController.graph).build()
         launchNavHost(coreViewModel.mainNavProvider, navController)
 
 
@@ -72,9 +77,9 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.app_bar, menu)
-        menu?.findItem(R.id.logout_button)?.setOnMenuItemClickListener {
+        menu.findItem(R.id.logout_button)?.setOnMenuItemClickListener {
             coreViewModel.onLogoutClick()
             true
         }
